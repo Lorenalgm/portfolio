b2e033c60a091be90e146d4863b4a97436aaab7b
Lorena <lorena@proesc.com>
2021-02-01 17:03:37 -0300

    Adicionados componentes que serão utilizados na home

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Project from '../../components/Project';
+import Video from '../../components/Video';
+import Article from '../../components/Article';
+import profile from '../../assets/profile.png';
 
 export default function Home(){
     return(
@@ -7,6 +11,22 @@ export default function Home(){
             <Header />
             <div className="hero-container">
                 <h1>Lorena</h1>
+                <img src={profile} alt="Lorena Góes Montes" />
+            </div>
+            <div className="projects">
+                <Project title="xxxxx" description="saasda" github_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+                <Project title="xxxxx" description="saasda" github_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+                <Project title="xxxxx" description="saasda" github_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+            </div>
+            <div className="videos">
+                <Video title="xxxxx" video_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+                <Video title="xxxxx" video_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+                <Video title="xxxxx" video_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+            </div>
+            <div className="articles">
+                <Project title="xxxxx" article_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+                <Project title="xxxxx" article_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
+                <Project title="xxxxx" article_url="https://github.com/Lorenalgm" image_url="https://i.imgur.com/7LVYmDo.png" />
             </div>
         </div>
     )
